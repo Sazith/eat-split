d4e211aa30f0e348fd87ac699a74b68f30c64ba2
shyontonsazith <mdsazithshyonton@gmail.com>
2024-10-19 10:02:23 +0600

    project completed

---
+++ src/Compnents/FriendsList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Friend from './Friend';
+
+const FriendsList = ({friends,selectedFriend,onSelection}) => {
+
+  return (
+    <>
+    <ul>
+      {
+        friends.map(friend => <Friend friend={friend} key={friend.id} selectedFriend={selectedFriend} onSelection={onSelection}/>)
+      }
+    </ul>
+      </>
+  )
+}
+
+export default FriendsList
